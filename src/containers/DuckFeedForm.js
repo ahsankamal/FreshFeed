14dea6e2298df267786b32d616e9173797c7eab4
Ahsan Kamal <ah721730@dal.ca>
2020-12-03 11:10:58 -0400

    enter duck feed form validations

--- src/containers/DuckFeedForm.js
+++ src/containers/DuckFeedForm.js
@@ -41,43 +41,49 @@ function DuckFeedForm() {
     <div className="form-group row d-flex justify-content-center">
       <label className="col-sm-2 col-form-label">Feeder Name</label>
       <div className="col-sm-3">
-        <input name="feeder" type="text" className="form-control" defaultValue="test" ref={register} />
+        <input name="feederName" type="text" className="form-control"  ref={register({required: 'required field', minLength: {value:3, message:'Name is too short'}, maxLength: {value:200, message:'Name is too long'}})} />
+        {errors.feederName && <span>{errors.feederName.message}</span>} 
       </div>
     </div>
 
     <div className="form-group row d-flex justify-content-center">
       <label className="col-sm-2 col-form-label ">Feeder Email</label>
       <div className="col-sm-3">
-        <input name="email" type="email" className="form-control" ref={register} />
+        <input name="email" type="email" className="form-control" placeHolder="Optional" ref={register} />
       </div>
     </div>
 
     <div className="form-group row d-flex justify-content-center">
       <label className="col-sm-2 col-form-label">Enter the number of ducks fed by you.</label>
       <div className="col-sm-3">
-        <input name="foodRecipientCount" type="number" className="form-control" ref={register} />
+        <input name="duckCount" type="number" className="form-control" ref={register({required: 'required field', min:{value:0, message:'Number of ducks fed cannot be negative'}})} />
+        {errors.duckCount && <span>{errors.duckCount.message}</span>} 
       </div>
     </div>
 
     <div className="form-group row d-flex justify-content-center">
       <label className="col-sm-2 col-form-label">What food the ducks were fed?</label>
       <div className="col-sm-3">
-        <input name="foodName" className="form-control" ref={register} />
+        <input name="foodName" type="text" className="form-control" ref={register({required: 'required field', minLength: {value:3, message:'Food Name is too short'}, maxLength: {value:200, message:'Food Name is too long'}})} />
+        {errors.foodName && <span>{errors.foodName.message}</span>} 
       </div>
         {/* <FoodAutoComplete /> */}
     </div>
 
     <div className="form-group row d-flex justify-content-center">
-      <label className="col-sm-2 col-form-label">How much amount of food were they fed? (lbs)</label>
+      <label className="col-sm-2 col-form-label">How much amount of food were they fed? (lb)</label>
       <div className="col-sm-3">
-        <input name="amount" type="number" className="form-control" ref={register} />
+        <input name="amount" type="number" step="0.1" className="form-control" placeHolder="Pounds (lb)" ref={register({required: 'required field', min:{value:0, message:'Amount of food cannot be negative'}})} />
+        {errors.amount && <span>{errors.amount.message}</span>}
       </div>
     </div>
 
+    {/* use long lat coordinates as input in future */}
     <div className="form-group row d-flex justify-content-center">
-      <label className="col-sm-2 col-form-label">Enter the park location where you fed the ducks.</label>
+      <label className="col-sm-2 col-form-label">Enter the park location/address where you fed the ducks.</label>
       <div className="col-sm-3">
-        <input name="location" className="form-control" ref={register} />
+        <input name="parkLocation" type="text" className="form-control" ref={register({required: 'required field', minLength: {value:15, message:'Please enter full address of park location'}, maxLength: {value:250, message:'Park location address is too long'}})} />
+        {errors.parkLocation && <span>{errors.parkLocation.message}</span>}
       </div>
     </div>
 
@@ -85,15 +91,14 @@ function DuckFeedForm() {
       <label className="col-sm-2 col-form-label">At what time did you feed the ducks?</label>
       <div className="col-sm-3">
         {/* <input name="time" className="form-control" ref={register} /> */}
-        <DateTimePicker name="time"/>
+        <DateTimePicker name="dateTime"/>
       </div>
     </div>
 
-    {/* <input name="exampleRequired" ref={register({required: 'required field', minLength: {value:7, message:'too short'}})} />
+    {/* <input name="exampleRequired" ref={register()} />
     {errors.exampleRequired && <span>{errors.exampleRequired.message}</span>} */}
     {/* <br></br> */}
 
-    {/* <input type="s  ubmit" /> */}
     <Button variant="primary" type="submit">
       Submit
     </Button>
